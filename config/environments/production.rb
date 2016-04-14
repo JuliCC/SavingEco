23f5ea75d4fbb7f4153a27d3d3a3907b9be80a7b
JuliCC <juli-c.c@hotmail.es>
2016-04-14 17:47:02 -0500

    Host option for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'savingeco.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['SENGRID_USERNAME'],
+    password:             ENV['SENGRID_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
