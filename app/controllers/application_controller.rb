ff0e64efc57d35006fe7403241de09e7e89979b1
JuliCC <juli-c.c@hotmail.es>
2016-04-21 17:23:11 -0500

    Update user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-  	 devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :lastname, :username, :code, :birthday, :occupation, :country, :email, :password, :password_confirmation) }
+  	 devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :lastname, :username, :code, :birthday, :occupation, :country, :email, :password, :password_confirmation, :avatar) }
   end
 
   def after_confirmation_path_for(resource_name, resource)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,16 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path
+    end
+
  def contact_params
   	params.require(:user).permit(:username)
   end
